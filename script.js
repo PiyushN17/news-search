20b945e35419346c98f4acf8e7e4510834edba79
Piyush Nath <142248814+PiyushN17@users.noreply.github.com>
2025-11-26 13:03:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,35 +8,33 @@ btn.addEventListener('click', function() {
         error.innerText = 'Please enter a search keyword.';
     }
     else {
-        
-        
         error.innerText = '';
         const input = search.value.trim();
-        const API_URL = `https://gnews.io/api/v4/search?q=${input}&lang=en&max=5&apikey=4711a47f13de7405829d73cddb08c47f`;
-        search.value = '';
+        const API_URL = `https://newsdata.io/api/1/latest?apikey=pub_b73f1a1c27d44867b0f9da3ceb4ba805&q=${input}`;
         async function newsWeb() {
             const output = await fetch(API_URL);
             const response = await output.json();
-            if(response.articles.length === 0) {
+            console.log(response)
+            if(response.results.length === 0) {
                 error.innerText = 'No news found for that keyword.';
             }
             else {
                 error.innerText = '';
                 wrap.innerHTML = '';
                 let num;
-                for(i = 0; i < response.articles.length; i++) {
-                        let element = document.createElement('div');
+                for(i = 0; i < response.results.length; i++) {
+                    let element = document.createElement('div');
                     element.className = 'card';
                     let title = document.createElement('h3');
-                    title.textContent = response.articles[i].title;
+                    title.textContent = response.results[i].title;
                     let desc = document.createElement('p');
-                    desc.innerText = response.articles[i].description;
+                    desc.innerText = response.results[i].description;
                     let image = document.createElement('img');
-                    image.src = response.articles[i].image;
+                    image.src = response.results[i].image_url;
                     image.alt = 'Image Not Available';
                     let source = document.createElement('a');
-                    source.innerText = response.articles[i].source.name;
-                    source.href = response.articles[i].url;
+                    source.innerText = response.results[i].source_name;
+                    source.href = response.results[i].link;
                     source.target = '_blank';
                     element.appendChild(image);
                     element.appendChild(source);
