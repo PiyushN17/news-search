1cdb960f029fae09e8b3e6968cf5d6ed6e19b47b
Piyush Nath <142248814+PiyushN17@users.noreply.github.com>
2025-11-26 12:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let btn = document.getElementById('searchBtn');
+let search = document.getElementById('search');
+let error = document.getElementById('error');
+let wrap = document.getElementById('wrap');
+
+btn.addEventListener('click', function() {
+    if(search.value.trim().length === 0) {
+        error.innerText = 'Please enter a search keyword.';
+    }
+    else {
+        error.innerText = '';
+        const input = search.value.trim();
+        const API_URL = 'https://newsapi.org/v2/everything?' + `q=${input}&`+ 'sortBy=popularity&'+'apiKey=d44d283396f04261b9466a45fe3d45e1';
+        search.value = '';
+        async function newsWeb() {
+            const output = await fetch(API_URL);
+            const response = await output.json();
+            if(response.articles.length === 0) {
+                error.innerText = 'No news found for that keyword.';
+            }
+            else {
+                error.innerText = '';
+                wrap.innerHTML = '';
+                let num;
+                for(i = 0; i < response.articles.length; i++) {
+                    let element = document.createElement('div');
+                    element.className = 'card';
+                    let title = document.createElement('h3');
+                    title.textContent = response.articles[i].title;
+                    let desc = document.createElement('p');
+                    desc.innerText = response.articles[i].description;
+                    let image = document.createElement('img');
+                    image.src = response.articles[i].urlToImage;
+                    image.alt = 'Image Not Available';
+                    let source = document.createElement('a');
+                    source.innerText = response.articles[i].source.name;
+                    source.href = response.articles[i].url;
+                    source.target = '_blank';
+                    element.appendChild(image);
+                    element.appendChild(source);
+                    element.appendChild(title);
+                    element.appendChild(desc);
+                    wrap.appendChild(element);
+                }
+            }
+            
+        }
+        newsWeb();
+    }
+})
+
