e0dce5fb16f59ef23f18cee8ff0f9fa6b80c4da1
Piyush Nath <142248814+PiyushN17@users.noreply.github.com>
2025-11-29 16:36:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>News Search</title>
-    <link rel="icon" href="https://i.fbcd.co/products/resized/resized-750-500/61f37e8f97ba85c20870422ac016b020c242cd36bca12185f3a9ebd86c2926ed.jpg">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    //Bootstrap used here
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </div>
-            </div>
-        </div>
-    </nav>
-    <h1>Search News</h1>
-    <input type="text" id="search" placeholder="Search News">
-    <button id="searchBtn">Search</button>
-    <p id="error"></p>
-    <div id="wrap">
-    </div>
-    <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
-</body>
-</html>
+let btn = document.getElementById('searchBtn');
+let search = document.getElementById('search');
+let error = document.getElementById('error');
+let wrap = document.getElementById('wrap');
 
+btn.addEventListener('click', function() {
+    if(search.value.trim().length === 0) {
+        error.innerText = 'Please enter a search keyword.';
+    }
+    else {
+        error.innerText = '';
+        const input = search.value.trim();
+        const API_URL = `https://newsdata.io/api/1/latest?apikey=pub_b73f1a1c27d44867b0f9da3ceb4ba805&q=${input}`;
+        async function newsWeb() {
+            const output = await fetch(API_URL);
+            const response = await output.json();
+            if(response.results.length === 0) {
+                error.innerText = 'No news found for that keyword.';
+            }
+            else {
+                error.innerText = '';
+                wrap.innerHTML = '';
+                let num;
+                for(i = 0; i < response.results.length; i++) {
+                    let element = document.createElement('div');
+                    element.className = 'card';
+                    let title = document.createElement('h3');
+                    title.textContent = response.results[i].title;
+                    let desc = document.createElement('p');
+                    desc.innerText = response.results[i].description;
+                    let image = document.createElement('img');
+                    image.src = response.results[i].image_url;
+                    image.alt = 'Image Not Available';
+                    let source = document.createElement('a');
+                    source.innerText = response.results[i].source_name;
+                    source.href = response.results[i].link;
+                    source.target = '_blank';
+                    element.appendChild(image);
+                    element.appendChild(source);
+                    element.appendChild(title);
+                    element.appendChild(desc);
+                    wrap.appendChild(element);
+                }
+            }
+            
+        }
+        newsWeb();
+    }
+})
